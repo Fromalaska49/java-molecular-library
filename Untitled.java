6c46caa374b509a6cf5397ce552c2d601d7a016c
Andrew Hutton <fromalaska49@gmail.com>
2016-05-02 22:44:59 -0500

    Prepped for testing

--- Untitled.java
+++ Untitled.java
@@ -2,13 +2,7 @@ public class Untitled{
 	
 	public static void main(String[] args){
 		String chemicalEquation = "NaCl2";
-		String molecule = simplifyMolecule(chemicalEquation);
-		if(isValidMolecule(molecule)){
-			//process molecule
-			
-		}
-		else{
-			System.out.println("Error: invalid molecule: "+molecule);
-		}
+		Molecule molecule = new Molecule(chemicalEquation);
+		System.out.println(molecule.getFormula());
 	}
 }
