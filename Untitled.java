d7c81de3d1734830fb32948786ce073eacf2defb
Andrew Hutton <fromalaska49@gmail.com>
2016-05-03 23:20:21 -0500

    Updated main with try catch for exceptions

--- Untitled.java
+++ Untitled.java
@@ -2,7 +2,18 @@ public class Untitled{
 	
 	public static void main(String[] args){
 		String chemicalEquation = "NaCl2";
-		Molecule molecule = new Molecule(chemicalEquation);
-		System.out.println(molecule.getFormula());
+		Molecule molecule = null;
+		try {
+			molecule = new Molecule(chemicalEquation);
+		} catch (MoleculeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			System.out.println(molecule.getFormula());
+		} catch (MoleculeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
