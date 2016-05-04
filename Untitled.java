54a04ab2403c1e508a53072afc9cc0b5a6d53622
Andrew Hutton <fromalaska49@gmail.com>
2016-05-03 23:24:47 -0500

    Changed test code to use a real molecule

--- Untitled.java
+++ Untitled.java
@@ -1,16 +1,16 @@
 public class Untitled{
 	
 	public static void main(String[] args){
-		String chemicalEquation = "NaCl2";
-		Molecule molecule = null;
+		String chemicalEquation = "CaCO3";
+		Molecule calciumCarbonate = null;
 		try {
-			molecule = new Molecule(chemicalEquation);
+			calciumCarbonate = new Molecule(chemicalEquation);
 		} catch (MoleculeException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		try {
-			System.out.println(molecule.getFormula());
+			System.out.println(calciumCarbonate.getFormula());
 		} catch (MoleculeException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
